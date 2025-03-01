87e6b1d2ae4a6b99808efb0ae9f40bd17ac8b5fa
桑榆肖物 <sang93@qq.com>
2025-03-01 14:03:25 +0800

    feat: 开发完成

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Sang.IoT.NV3030B
+
+NV3030B LCD Controller
+
+## Introduction
+
+This is a library for the NV3030B LCD controller. It is used in the Waveshare 1.5inch LCD Module.
+
+This library is based on the [Ili934x](https://github.com/dotnet/iot/tree/main/src/devices/Ili934x?wt.mc_id=DT-MVP-5005195) library.
+
+## Installation
+
+You can install this library:
+
+```bash
+dotnet add package Sang.IoT.NV3030B
+```
+
+## Usage
+
+```csharp
+using Iot.Device.Graphics;
+using Iot.Device.Graphics.SkiaSharpAdapter;
+using Sang.IoT.NV3030B;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.Device.Spi;
+
+// Setup the GPIO
+const int pinID_DC = 25;
+const int pinID_Reset = 27;
+const int pinID_BL = 18;
+
+SkiaSharpAdapter.Register();
+
+// Setup the SPI
+SpiDevice displaySPI = SpiDevice.Create(new SpiConnectionSettings(0, 0)
+{
+    Mode = SpiMode.Mode0,
+    DataBitLength = 8,
+    ClockFrequency = 40_000_000
+});
+
+// Create the display
+using var display = new NV3030B(displaySPI, pinID_DC, pinID_Reset, pinID_BL);
+
+// Show a bitmap
+using var image = BitmapImage.CreateFromFile("image.jpg");
+display.DrawBitmap(image);
+```
+
+## Hardware
+
+The NV3030B is a LCD controller used in the Waveshare 1.5inch LCD Module. It is connected to the Raspberry Pi using SPI.
+
+## References
+
+- [Waveshare 1.5inch LCD Module](https://www.waveshare.com/wiki/1.5inch_LCD_Module)
+- [Ili934x](https://github.com/dotnet/iot/tree/main/src/devices/Ili934x?wt.mc_id=DT-MVP-5005195)
---
+++ src/NV3030BasicGraphics.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Iot.Device.Graphics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+namespace Sang.IoT.NV3030B
+{
+    public partial class NV3030B
+    {
+        /// <summary>
+        /// Fill rectangle to the specified color
+        /// </summary>
+        /// <param name="color">The color to fill the rectangle with.</param>
+        /// <param name="x">The x co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="y">The y co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="w">The width of the rectangle in pixels.</param>
+        /// <param name="h">The height of the rectangle in pixels.</param>
+        public void FillRect(Color color, int x, int y, int w, int h)
+        {
+            FillRect(color, x, y, w, h, false);
+        }
+
+        /// <summary>
+        /// Fill rectangle to the specified color
+        /// </summary>
+        /// <param name="color">The color to fill the rectangle with.</param>
+        /// <param name="x">The x co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="y">The y co-ordinate of the point to start the rectangle at in pixels.</param>
+        /// <param name="w">The width of the rectangle in pixels.</param>
+        /// <param name="h">The height of the rectangle in pixels.</param>
+        /// <param name="doRefresh">True to immediately update the screen, false to only update the back buffer</param>
+        private void FillRect(Color color, int x, int y, int w, int h, bool doRefresh)
+        {
+            Span<byte> colourBytes = stackalloc byte[2]; // create a short span that holds the colour data to be sent to the display
+
+            // set the colourbyte array to represent the fill colour
+            var c = Rgb565.FromRgba32(color);
+
+            // set the pixels in the array representing the raw data to be sent to the display
+            // to the fill color
+            for (int j = y; j < y + h; j++)
+            {
+                for (int i = x; i < x + w; i++)
+                {
+                    _screenBuffer[i + j * ScreenWidth] = c;
+                }
+            }
+
+            if (doRefresh)
+            {
+                SendFrame(false);
+            }
+        }
+
+        /// <summary>
+        /// Clears the screen to a specific color
+        /// </summary>
+        /// <param name="color">The color to clear the screen to</param>
+        /// <param name="doRefresh">Immediately force an update of the screen. If false, only the backbuffer is cleared.</param>
+        public void ClearScreen(Color color, bool doRefresh)
+        {
+            FillRect(color, 0, 0, ScreenWidth, ScreenHeight, doRefresh);
+        }
+
+        /// <summary>
+        /// Clears the screen to black
+        /// </summary>
+        /// <param name="doRefresh">Immediately force an update of the screen. If false, only the backbuffer is cleared.</param>
+        public void ClearScreen(bool doRefresh)
+        {
+            FillRect(Color.FromArgb(0, 0, 0), 0, 0, ScreenWidth, ScreenHeight, doRefresh);
+        }
+    }
+}
